2963067c3979aa6478e9c11fa601ccfb5d5cae86
Ryan Balshaw <ryan@kriterion.ai>
2024-10-03 17:17:16 +0200

    wip(pretty_plots): Incorporated font-awesome and finalised plot

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import matplotlib.font_manager as fm
 import numpy as np
 import scipy.stats as scistats
 from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import NYARPR.StravaVisualiser as strava_vis
@@ -67,17 +68,6 @@ if __name__ == "__main__":
         # Check the tokens
         strava_vis.check_tokens(env_path)
 
-    # print(strava_vis.get_env_variables(env_path))
-
-    # df = strava_vis.get_activities(env_path)
-
-    # df.to_cstrava_vis('strava_activities.cstrava_vis')
-
-    # TODO
-    # Create a get_latest_activity_code function - done
-    # Create a get all user information function - done
-    # Visualise it all on one grid - use matplotlib!
-
     df_cumulative_info = strava_vis.get_cumulative_information(env_path)
 
     recent_activity_id = strava_vis.get_latest_activity_code(
@@ -93,44 +83,10 @@ if __name__ == "__main__":
     hrt = df_recent_activity_stream["heartrate.data"].iloc[0]
     lat_lng = np.array(df_recent_activity_stream["latlng.data"].iloc[0])
     alt = df_recent_activity_stream["altitude.data"].iloc[0]
+    rel_alt = np.array(alt)
+    rel_alt -= rel_alt[0]
 
-    # Visualise heart rate trend
-    # fig, ax = plt.subplots(2, 1)
-    # ax[0].plot(t, hrt, color="b")
-    # ax[0].set_ylabel("heartrate (BPM)")
-    #
-    # ax[1].plot(t, alt, color="k")
-    # ax[1].set_ylabel("Altitude (metres)")
-    #
-    # for axs in ax:
-    #     axs.grid()
-    #     axs.set_xlabel("Time (minutes)")
-    # plt.show()
-    #
-    # plt.figure()
-    #
-    # counts, bins = np.histogram(hrt, density=True, bins=50)
-    # plt.hist(bins[:-1], bins, weights=counts, color="b", alpha=0.6)
-    #
-    # kde = scistats.gaussian_kde(hrt)
-    # xx = np.linspace(np.min(hrt), np.max(hrt), 100)
-    # plt.plot(xx, kde(xx), color="r", linestyle="--")
-    # plt.xlabel("heartrate (BPM)")
-    # plt.ylabel("Density")
-    # plt.grid()
-    # plt.show()
-    #
-    # plt.figure()
-    # plt.plot(lat_lng[:, 0], lat_lng[:, 1], color="b")
-    # plt.grid()
-    # plt.xlabel("Latitude")
-    # plt.ylabel("Longitude")
-    # plt.show()
-
-    # Calculate gradient
-    gradient = np.gradient(alt, t)
-
-    # Set up Roboto font
+    # Set up Roboto font and Font Awesome
     roboto_regular = fm.FontProperties(
         fname=os.path.join(os.getcwd(), "fonts", "roboto", "Roboto-Regular.ttf")
     )
@@ -139,15 +95,7 @@ if __name__ == "__main__":
     )
     font_awesome = fm.FontProperties(
         fname=os.path.join(
-            os.getcwd(), "icons", "font-awesome", "Font Awesome 6 Free-Regular-400.otf"
-        )
-    )
-    font_awesome_brands = fm.FontProperties(
-        fname=os.path.join(
-            os.getcwd(),
-            "icons",
-            "font-awesome",
-            "Font Awesome 6 Brands-Regular-400.otf",
+            os.getcwd(), "icons", "font-awesome", "Font Awesome 6 Free-Solid-900.otf"
         )
     )
 
@@ -166,54 +114,99 @@ if __name__ == "__main__":
             "axes.labelcolor": text_color,
             "xtick.color": text_color,
             "ytick.color": text_color,
+            "figure.dpi": 300,
         }
     )
 
+    _label_fontsize = 16
+    _title_fontsize = 20
+    _text_fontsize = 14
+    _icon_fontsize = 18
+    _tick_size = 12
+
     # Create the plot
-    fig = plt.figure(figsize=(14, 12))
+    fig = plt.figure(figsize=(16, 12))
     gs = fig.add_gridspec(
-        2, 2, width_ratios=[4, 1], height_ratios=[2, 1], hspace=0.3, wspace=0.3
+        2, 2, width_ratios=[3, 1], height_ratios=[2, 1], hspace=0.3, wspace=0.3
     )
 
-    # Run path colored by gradient
+    # Run path colored by heart rate
     ax_path = fig.add_subplot(gs[0, :])
     scatter = ax_path.scatter(
-        lat_lng[:, 1], lat_lng[:, 0], c=gradient, cmap="viridis", s=3, alpha=0.7
+        lat_lng[:, 1], lat_lng[:, 0], c=rel_alt, cmap="magma", s=10, alpha=0.7
     )
-    ax_path.set_xlabel("Longitude", fontproperties=roboto_regular, fontsize=12)
-    ax_path.set_ylabel("Latitude", fontproperties=roboto_regular, fontsize=12)
+    ax_path.set_xlabel(
+        "Longitude", fontproperties=roboto_regular, fontsize=_label_fontsize
+    )
+    ax_path.set_ylabel(
+        "Latitude", fontproperties=roboto_regular, fontsize=_label_fontsize
+    )
     ax_path.set_title(
-        "Run df_recent_activity_stream", fontproperties=roboto_bold, fontsize=14
+        "Latest run:", fontproperties=roboto_bold, fontsize=_title_fontsize
     )
-    ax_path.tick_params(axis="both", which="major", labelsize=10)
+    ax_path.axis("off")
 
     # Add colorbar
     divider = make_axes_locatable(ax_path)
     cax = divider.append_axes("right", size="5%", pad=0.1)
     cbar = plt.colorbar(scatter, cax=cax)
-    cbar.set_label("Gradient", fontproperties=roboto_regular, fontsize=12)
-    cbar.ax.tick_params(labelsize=10)
+    cbar.set_label(
+        "Altitude (relative)", fontproperties=roboto_regular, fontsize=_label_fontsize
+    )
+    cbar.ax.tick_params(labelsize=_tick_size)
 
-    # Add distance and date to legend
-    recent_distance = df_cumulative_info["recent_run_totals.distance"].iloc[0] / 1000
+    # Add run details to legend
+    recent_distance = df_recent_activity_info["distance"].iloc[0] / 1000
     recent_timestamp = df_recent_activity_info["start_date_local"].iloc[0]
     recent_gear = df_recent_activity_info["gear.name"].iloc[0]
     recent_calories = df_recent_activity_info["calories"].iloc[0]
+    recent_moving_time = df_recent_activity_info["moving_time"].iloc[0] / 60
 
     dt = datetime.strptime(recent_timestamp, "%Y-%m-%dT%H:%M:%SZ")
     formatted_recent_timestamp = dt.strftime("%d %b %Y, %I:%M %p")
 
-    ax_path.text(
-        0.02,
-        0.98,
-        f"Latest Run: {formatted_recent_timestamp}\nDistance: {recent_distance:.2f} km\nGear: {recent_gear}\nCalories: {recent_calories}",
-        transform=ax_path.transAxes,
-        fontproperties=roboto_regular,
-        fontsize=10,
-        verticalalignment="top",
-        color=text_color,
-        bbox=dict(facecolor=bg_color, edgecolor=accent_color, alpha=0.7),
-    )
+    details = [
+        ("\uf073", f"{formatted_recent_timestamp}", "Date"),
+        ("\uf70c", f"{recent_distance:.2f} km", "Distance"),
+        ("\uf013", f" {recent_gear}", "Gear"),
+        ("\uf06d", f"{recent_calories:.0f} kcal", "Calories"),
+        ("\uf017", f"{recent_moving_time:.2f} min.", "Moving time"),
+    ]
+
+    for i, (icon, value, label) in enumerate(details):
+        ax_path.text(
+            0.02,
+            0.98 - i * 0.1,
+            icon,
+            transform=ax_path.transAxes,
+            fontproperties=font_awesome,
+            fontsize=_icon_fontsize,
+            va="top",
+            ha="center",
+            color=accent_color,
+        )
+        ax_path.text(
+            0.06,
+            0.98 - i * 0.1,
+            value,
+            transform=ax_path.transAxes,
+            va="top",
+            ha="left",
+            fontproperties=roboto_bold,
+            fontsize=_text_fontsize,
+            color=text_color,
+        )
+        ax_path.text(
+            0.06,
+            0.94 - i * 0.1,
+            label,
+            transform=ax_path.transAxes,
+            va="top",
+            ha="left",
+            fontproperties=roboto_regular,
+            fontsize=_text_fontsize - 2,
+            color=secondary_color,
+        )
 
     # Heart rate KDE and histogram
     ax_hr = fig.add_subplot(gs[1, 0])
@@ -228,39 +221,85 @@ if __name__ == "__main__":
     kde = scistats.gaussian_kde(hrt)
     xx = np.linspace(np.min(hrt), np.max(hrt), 100)
     ax_hr.plot(xx, kde(xx), color=accent_color, linestyle="--", linewidth=2)
-    ax_hr.set_xlabel("Heart Rate (BPM)", fontproperties=roboto_regular, fontsize=12)
-    ax_hr.set_ylabel("Density", fontproperties=roboto_regular, fontsize=12)
-    ax_hr.set_title("Heart Rate Distribution", fontproperties=roboto_bold, fontsize=14)
-    ax_hr.tick_params(axis="both", which="major", labelsize=10)
+    ax_hr.set_xlabel(
+        "Heart rate (BPM)", fontproperties=roboto_regular, fontsize=_label_fontsize
+    )
+    ax_hr.set_ylabel("Density", fontproperties=roboto_regular, fontsize=_label_fontsize)
+    ax_hr.set_title(
+        "Heart rate distribution", fontproperties=roboto_bold, fontsize=_title_fontsize
+    )
+    ax_hr.tick_params(axis="both", which="major", labelsize=_tick_size)
+    ax_hr.yaxis.set_major_locator(MaxNLocator(nbins=3))
+    ax_hr.xaxis.set_major_locator(MaxNLocator(nbins=5))
 
     # Stats bar
     ax_stats = fig.add_subplot(gs[1, 1])
     ax_stats.text(
         0.5,
+        0.9,
+        "\uf201",
+        fontproperties=font_awesome,
+        fontsize=_icon_fontsize * 1.5,
+        va="center",
+        ha="center",
+        color=accent_color,
+    )
+    ax_stats.text(
+        0.5,
         0.8,
-        "Yearly Statistics:",
+        "Yearly statistics",
         va="center",
         ha="center",
         fontproperties=roboto_bold,
-        fontsize=12,
+        fontsize=_text_fontsize,
         color=text_color,
     )
+
     stats = [
-        f"Runs: {df_cumulative_info['ytd_run_totals.count'].iloc[0]}",
-        f"Distance: {df_cumulative_info['ytd_run_totals.distance'].iloc[0] / 1000:.1f} km",
-        f"Time spent: {df_cumulative_info['ytd_run_totals.moving_time'].iloc[0] / 3600:.1f} hours",
+        ("\uf70c", f"{df_cumulative_info['ytd_run_totals.count'].iloc[0]}", "Runs"),
+        (
+            "\uf547",
+            f"{df_cumulative_info['ytd_run_totals.distance'].iloc[0] / 1000:.1f} km",
+            "Distance",
+        ),
+        (
+            "\uf017",
+            f"{df_cumulative_info['ytd_run_totals.moving_time'].iloc[0] / 3600:.1f} hours",
+            "Time spent",
+        ),
     ]
-    ax_stats.text(
-        0.5,
-        0.5,
-        "\n".join(stats),
-        va="center",
-        ha="center",
-        fontproperties=roboto_regular,
-        fontsize=12,
-        color=text_color,
-    )
-    ax_stats.text(0.6, 0.4, "\uf428", fontproperties=font_awesome_brands)
+
+    for i, (icon, value, label) in enumerate(stats):
+        ax_stats.text(
+            0.1,
+            0.6 - i * 0.2,
+            icon,
+            fontproperties=font_awesome,
+            fontsize=_icon_fontsize,
+            va="center",
+            color=accent_color,
+        )
+        ax_stats.text(
+            0.3,
+            0.6 - i * 0.2,
+            value,
+            va="center",
+            ha="left",
+            fontproperties=roboto_bold,
+            fontsize=_text_fontsize,
+            color=text_color,
+        )
+        ax_stats.text(
+            0.3,
+            0.53 - i * 0.2,
+            label,
+            va="center",
+            ha="left",
+            fontproperties=roboto_regular,
+            fontsize=_text_fontsize - 2,
+            color=secondary_color,
+        )
+
     ax_stats.axis("off")
 
     # Add a border to the stats box
@@ -269,14 +308,5 @@ if __name__ == "__main__":
     ax_stats.patch.set_facecolor(bg_color)
     ax_stats.patch.set_alpha(0.3)
 
-    # Add main title
-    fig.suptitle(
-        "Strava Run Analysis",
-        fontproperties=roboto_bold,
-        fontsize=16,
-        y=0.98,
-        color=text_color,
-    )
-
-    plt.tight_layout()
+    fig.tight_layout()
     plt.show()
